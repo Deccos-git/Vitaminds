33eeb10c64991fbf24487d5877db47e4988f9f23
Gijs van Beusekom <Gijs@Gijss-MacBook-Air.local>
2019-12-19 12:01:05 +0100

    post werkt!

--- Public/javascript/coaches.js
+++ Public/javascript/coaches.js
@@ -1,8 +1,4 @@
 
-document.body.addEventListener('click', () => {
-    db.collection("Vitaminders").where("Locatie", "==", 'Meppel').then(console.log);
-});
-
 // Coaches inladen in overzicht
 
 db.collection("Vitaminders").where("Usertype", "==", "Coach")
@@ -52,99 +48,47 @@ db.collection("Vitaminders").where("Usertype", "==", "Coach")
         nieuweDiv.appendChild(link)
     
         })
-    }).catch(function(error) {
+    }).catch((error) => {
         console.log("Kan de coaches niet inladen: ", error);
     }) 
 
 
 //Filter
-function zoek(){
-const stad = document.getElementById("filterStad");
-const stadOpties = stad.options;
-const stadSelect = stadOpties[stadOpties.selectedIndex].value;
+const inputCoachCategorien = document.getElementById("filterCoachCategorien")
+                 const inputOptiesCC = inputCoachCategorien.options;
+                 const inputSelectCC = inputOptiesCC[inputOptiesCC.selectedIndex].value;    
 
-const thema = document.getElementById("filterThema");
-const themaOpties = thema.options;
-const themaSelect = themaOpties[themaOpties.selectedIndex].value;
+const inputDoelgroep = document.getElementById("filterDoelgroep")
+                 const inputOptiesDD = inputDoelgroep.options;
+                 const inputSelectDD = inputOptiesDD[inputOptiesDD.selectedIndex].value;    
 
-const doelgroep = document.getElementById("filterDoelgroep");
-const doelgroepOpties = doelgroep.options;
-const doelgroepSelect = doelgroepOpties[doelgroepOpties.selectedIndex].value;
+const inputLocatie = document.getElementById("filterLocatie")
+                 const inputOptiesLC = inputLocatie.options;
+                 const inputSelectLC = inputOptiesLC[inputOptiesLC.selectedIndex].value;   
 
-if (stadSelect == "Null", themaSelect == "Null" , doelgroepSelect == "Null"){
-    db.collection("Vitaminders").where("Type", '==', "Coach")
-} else {
+const dataSet = [];
 
-db.collection("Vitaminders").where("Locatie", "==", stadSelect)
-.where("CoachCategorien", "==", themaSelect)
-.where("Doelgroep", "==", doelgroepSelect)
-.get()
-.then(querySnapshot => {
-    querySnapshot.forEach(doc => {
-
-        console.log(doc)
-
-        if(doc.exists){
-
-        //Waar de coachprofielen onder komen in de DOM
-        const overview = document.getElementById("overview");
-
-            //De coach-eigenschappen
-        const naam = doc.data().Gebruikersnaam;
-        const stijl = doc.data().Coachstijl
-        const omschrijving = doc.data().Omschrijving
-
-            // De nieuwe HTML-elementen en classes
-        const filterDiv = document.createElement("div");
-            filterDiv.setAttribute("class", "nieuweD")
-        const profielFoto = document.createElement("div")
-            profielFoto.setAttribute("class", "profielfotoCH")
-        const nieuweH3 = document.createElement("h3");
-            nieuweH3.setAttribute("class", "coachnaam");
-        const stijlCH = document.createElement("p")
-        const omschrijvingCH = document.createElement("h4");
-        const link = document.createElement("button");
-            link.setAttribute("class", "button-algemeen")
+function getFilterItems({
+    CoachCategorien, Locatie, Doelgroep}){
+        const filters = {
+            CoachCategorien: [],
+            Locatie: [],
+            Doelgroep: []
+        };
         
-            nieuweH3.addEventListener('click', (e) => {
-                window.open("Vitaminders/" + [naam] + ".html", "_self");
-            })
-
-            link.addEventListener('click', (e) => {
-                window.open("Vitaminders/" + [naam] + ".html", "_self");
-            })   
-
-            //De coach-eigenschappen in de nieuwe HTML-elementen zetten
-        nieuweH3.innerHTML = naam;
-        stijlCH.innerHTML = stijl;   
-        omschrijvingCH.innerHTML ='"' + omschrijving + '"'
-        link.innerHTML = "Bekijk mijn profiel"
-
-            //Oude div verwijderen
-        const oudeDiv = document.querySelectorAll(".nieuweD")
-        oudeDiv.forEach(D =>{
-            D.style.display = "none"
+        dataSet.forEach(data => {
+           if(!filters.CoachCategorien.includes(data.CoachCategorien)){
+                filters.CoachCategorien.push(data.CoachCategorien)
+           }
         })
-
-            //De HTML-elementen vastmaken aan de DOM
-        overview.appendChild(filterDiv)
-        filterDiv.appendChild(profielFoto)
-        filterDiv.appendChild(nieuweH3)
-        filterDiv.appendChild(stijlCH)
-        filterDiv.appendChild(omschrijvingCH)
-        filterDiv.appendChild(link)
-
-        }else{
-            const oudeDiv = document.querySelectorAll(".nieuweD")
-        oudeDiv.forEach(D =>{
-            D.style.display = "none"
-            
-        })
-        }
-        })
-    
-    }).catch(function(error) {
-        console.log("Kan de coaches niet inladen: ", error);
-    })
+        return filters;        
 }
-};
+
+db.collection("Vitaminders").where("Usertype", "==", "Coach").get().then((querySnapshot) => {
+    querySnapshot.forEach( doc =>{
+        dataSet.push(
+            doc.data()
+        );
+    });
+     console.log(getFilterItems({}))
+})
--- app.js
+++ app.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser')
 const port = process.env.port || 5500
-
-app.listen(port)
 
 const fs = require("fs")
 const html = "<h1>Test</h1>"
 
-app.post("/", (res, req) => {
-fs.writeFile("Public/Vitaminders/Naam.html", html, (err) => {
-  console.log(err)
-  })
+app.use(express.static('Public'))
+
+
+app.post("/api/user", (res, req) => {
+  console.log(req.body)
+// fs.writeFile("Public/Vitaminders/2.html", html, (err) => {
+//   console.log(err)
+//   })
 })
 
-
+app.listen(port)
