03c6e90339b8e664604f6966cd23c871748fd59d
Deccos <gvbeusekom84u@hotmai.com>
2020-07-21 22:13:40 +0200

    20-7-2020 II

--- Public/javascript/openup.js
+++ Public/javascript/openup.js
@@ -179,7 +179,6 @@ const DOM = document.getElementById("verzamelOpenUps")
 // Check in
 
     const checkInSelect = document.getElementById("check-in-select")
-    const checkInTitle = document.getElementById("check-in-titel")  
     const fotoUploadCheckIn = document.getElementById("foto-upload-check-in")
     const checkInShareButton = document.getElementById("check-in-button")
     const selectGoalTitle = document.getElementById("select-goal-check-in") 
@@ -207,7 +206,7 @@ const DOM = document.getElementById("verzamelOpenUps")
                 const gebruikersnaam = doc.data().Gebruikersnaam
                 const gebruikersnaamClean = doc.data().GebruikersnaamClean
 
-                checkInTitle.innerText = `Hoe gaat het met je, ${gebruikersnaamClean} ?`
+                inputCheckIn.placeholder = `Hoe gaat het met je, ${gebruikersnaamClean} ?`
 
                 db.collectionGroup("Levensvragen").where("Gebruikersnaam", "==", gebruikersnaam).where("Openbaar", "==", "Ja").get().then(querySnapshot =>{
                     querySnapshot.forEach(doc1 => {
@@ -236,20 +235,43 @@ const DOM = document.getElementById("verzamelOpenUps")
         const selectedFile = document.getElementById('foto-upload-check-in').files[0];
         const progressBar = document.getElementById("progress-bar")
 
-        if (selectedFile == undefined){
+        if (selectedFile == undefined && selected == "Niet aan doel koppelen"){
             db.collection("Vitaminders").doc(User.uid).collection("Levenslessen").doc().set({
                 Gebruikersnaam: gebruikersnaam,
                 Timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
                 Inspirerend: 1,
-                Type: "Check-in",
+                Type: "Update",
                 Levensles: inputCheckIn.value,
                 Levensvraag: "Geen",
                 BackgroundImage: "Geen"
                 });
-        } else {
+        } else if (selectedFile == undefined && selected != "Niet aan doel koppelen") {
+             //LevensvraagClean naar levensvraag met ID
+                
+             db.collectionGroup("Levensvragen").where("LevensvraagClean", "==", selected).where("Gebruikersnaam", "==", gebruikersnaam).get().then(querySnapshot =>{
+                querySnapshot.forEach(doc2 => {
+    
+                    const levensvraagID = doc2.data().Levensvraag
+    
+        db.collection("Vitaminders").doc(User.uid).collection("Levenslessen").doc().set({
+            Gebruikersnaam: gebruikersnaam,
+            Timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
+            Inspirerend: 1,
+            Type: "Check-in",
+            Levensles: inputCheckIn.value,
+            Levensvraag: levensvraagID,
+            BackgroundImage: "Geen"
+                                    });
+    
+        db.collection("Vitaminders").doc(User.uid).collection("Levensvragen").doc(doc2.id).update({
+            Levenslessen: firebase.firestore.FieldValue.arrayUnion(inputCheckIn.value)
+        })
+                                            });
+                                        });
 
-        
+            } else if (selectedFile != undefined && selected == "Niet aan doel koppelen") {
 
+                console.log("test")
         const storageRef = firebase.storage().ref("/Check-in-photos/" + selectedFile.name);
 
         const uploadTask = storageRef.put(selectedFile)
@@ -278,48 +300,85 @@ const DOM = document.getElementById("verzamelOpenUps")
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
         console.log('File available at', downloadURL);
-
-        if(selected == "Niet aan doel koppelen"){
-
-            db.collection("Vitaminders").doc(User.uid).collection("Levenslessen").doc().set({
-                Gebruikersnaam: gebruikersnaam,
-                Timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
-                Inspirerend: 1,
-                Type: "Update",
-                Levensles: inputCheckIn.value,
-                Levensvraag: "Geen",
-                BackgroundImage: downloadURL
-                });
-
-        } else {
+        
 
         //LevensvraagClean naar levensvraag met ID
-
-        db.collectionGroup("Levensvragen").where("LevensvraagClean", "==", selected).where("Gebruikersnaam", "==", gebruikersnaam).get().then(querySnapshot =>{
-            querySnapshot.forEach(doc2 => {
-
-                const levensvraagID = doc2.data().Levensvraag
 
     db.collection("Vitaminders").doc(User.uid).collection("Levenslessen").doc().set({
         Gebruikersnaam: gebruikersnaam,
         Timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
         Inspirerend: 1,
-        Type: "Check-in",
+        Type: "Update",
         Levensles: inputCheckIn.value,
-        Levensvraag: levensvraagID,
+        Levensvraag: "Geen",
         BackgroundImage: downloadURL
-                                });
-
-    db.collection("Vitaminders").doc(User.uid).collection("Levensvragen").doc(doc2.id).update({
-        Levenslessen: firebase.firestore.FieldValue.arrayUnion(inputCheckIn.value)
-    })
-                                            });
-                                        });
-                                    };
+                                    });
                                 });                                                
                             });
                         });
+                    } else {
+
+                        const storageRef = firebase.storage().ref("/Check-in-photos/" + selectedFile.name);
+
+                        const uploadTask = storageRef.put(selectedFile)
+                        uploadTask.then(() => {
+                        // Register three observers:
+                        // 1. 'state_changed' observer, called any time the state changes
+                        // 2. Error observer, called on failure
+                        // 3. Completion observer, called on successful completion
+                        uploadTask.on('state_changed', function(snapshot){
+                        // Observe state change events such as progress, pause, and resume
+                        // Get task progress, including the number of bytes uploaded and the total number of bytes to be uploaded
+                        var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                        progressBar.innerHTML = ` ${progress} %`;
+                        switch (snapshot.state) {
+                        case firebase.storage.TaskState.PAUSED: // or 'paused'
+                            console.log('Upload is paused');
+                            break;
+                        case firebase.storage.TaskState.RUNNING: // or 'running'
+                            console.log('Upload is running');
+                            break;
+                        }
+                        }, function(error) {
+                        // Handle unsuccessful uploads
+                        }, function() {
+                        // Handle successful uploads on complete
+                        // For instance, get the download URL: https://firebasestorage.googleapis.com/...
+                        uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
+                        console.log('File available at', downloadURL);
+                        
+                
+                        //LevensvraagClean naar levensvraag met ID
+                
+                        db.collectionGroup("Levensvragen").where("LevensvraagClean", "==", selected).where("Gebruikersnaam", "==", gebruikersnaam).get().then(querySnapshot =>{
+                            querySnapshot.forEach(doc2 => {
+                
+                                const levensvraagID = doc2.data().Levensvraag
+                
+                    db.collection("Vitaminders").doc(User.uid).collection("Levenslessen").doc().set({
+                        Gebruikersnaam: gebruikersnaam,
+                        Timestamp: firebase.firestore.Timestamp.fromDate(new Date()),
+                        Inspirerend: 1,
+                        Type: "Check-in",
+                        Levensles: inputCheckIn.value,
+                        Levensvraag: levensvraagID,
+                        BackgroundImage: downloadURL
+                                                });
+                
+                    db.collection("Vitaminders").doc(User.uid).collection("Levensvragen").doc(doc2.id).update({
+                        Levenslessen: firebase.firestore.FieldValue.arrayUnion(inputCheckIn.value)
+                    })
+                                                        });
+                                                    });
+                                                });                                                
+                                            });
+                                        });
+
                     }
+
+                    const sharedNotification = document.getElementById("shared-notification")
+
+                    sharedNotification.style.display = "block"
                 });
             });
     } else {
